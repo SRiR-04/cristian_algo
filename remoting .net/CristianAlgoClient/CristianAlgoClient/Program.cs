207afa0671a4e77dca57dd327972e5e36ce496b6
Adrian Chmura <adrian.chmura@pega.com>
2016-06-08 17:40:58 +0200

    Uploaded .NET Remoting version of christian's algorithm

---
+++ remoting .net/CristianAlgoClient/CristianAlgoClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace CristianAlgoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            TcpChannel tcpChannel = new TcpChannel();
+            ChannelServices.RegisterChannel(tcpChannel, true);
+
+            Type requiredType = typeof(CristianInterface);
+
+            CristianInterface remoteObject = (CristianInterface)Activator.GetObject(requiredType,
+            "tcp://localhost:9998/GetTime");
+
+            DateTime clientTime1 = DateTime.Now;
+            DateTime serverTime = remoteObject.GetServerTime();
+            DateTime clientTime2 = DateTime.Now;           
+            
+            TimeSpan diff = serverTime - clientTime1;
+            TimeSpan diff2 = clientTime2 - serverTime;
+
+            int ping = (diff.Milliseconds + diff2.Milliseconds) / 2;
+
+            DateTime newTime = serverTime.AddMilliseconds(ping);
+            Console.WriteLine("Server time: " + serverTime + ":" + serverTime.Millisecond);
+            Console.WriteLine("Time of connection (in miliseconds) " + ping);
+            Console.WriteLine("New client time base on Cristian's Alghoritm: " + newTime +":"+newTime.Millisecond);
+            Console.ReadKey();
+        }
+    } 
+}
+
+public interface CristianInterface
+{
+    DateTime GetServerTime();
+}
---
+++ remoting .net/CristianAlgoServer/CristianAlgoServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+namespace CristianAlgoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Server started...");
+
+            TcpChannel tcpChannel = new TcpChannel(9998);
+            ChannelServices.RegisterChannel(tcpChannel, true);
+
+            Type commonInterfaceType = Type.GetType("Cristian");
+            
+
+            RemotingConfiguration.RegisterWellKnownServiceType(commonInterfaceType,
+            "GetTime", WellKnownObjectMode.SingleCall);
+
+            System.Console.WriteLine("Press ENTER to quit");
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
+
+public interface CristianInterface
+{
+    DateTime GetServerTime();
+}
+
+public class Cristian : MarshalByRefObject, CristianInterface
+{
+    public DateTime GetServerTime()
+    {
+       // Console.WriteLine("Enquiry for {0} to get server time.");       
+        return DateTime.Now;
+    }
+}
